7543f408cb7b14eedea72a0ed40befc14c63d65e
Showmick Das <showmickdas75@gmail.com>
2025-07-21 04:48:25 +0600

    Updated structure

--- tests/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Test package for the Transformer implementation.""" 
--- transformer/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Transformer implementation package."""
-from .config import TransformerConfig
-from .model import Transformer
-from .loss import LabelSmoothingLoss, masked_loss
-
-__version__ = '0.1.0'
-__all__ = ['TransformerConfig', 'Transformer', 'LabelSmoothingLoss', 'masked_loss'] 
